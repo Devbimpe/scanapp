569f530c4f1899110da137976208b923d501482c
Bimpe <ayoolabimpe@gmail.com>
2018-08-09 16:43:40 +0100

    add

---
+++ src/main/java/com/scanapp/ScanappApplication.java
@@ -0,0 +1,12 @@
+package com.scanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanappApplication.class, args);
+	}
+}
