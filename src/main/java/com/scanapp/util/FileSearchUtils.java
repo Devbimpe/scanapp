f3cf5b95d1df62a3a35143a85d8c026ff5923a3c
oluexpert <ayoade_farooq@yahoo.com>
2018-08-16 15:25:53 +0100

    latest update

--- src/main/java/com/scanapp/util/FileSearchUtils.java
+++ src/main/java/com/scanapp/util/FileSearchUtils.java
@@ -67,7 +67,7 @@ public class FileSearchUtils implements Serializable {
                         if (accept(temp)) {
                             result.add(temp.getAbsoluteFile().toString());
 
-                            List<String> cards = sparkWordCount(temp.getAbsoluteFile().toString());
+                            Set<String> cards = getCreditCardsFromFile(temp.getAbsoluteFile().toString());
                             if (cards.size() > 0) {
                                 try {
                                     writeToFile(cards.stream().collect(Collectors.toList()), temp.getAbsoluteFile().toString());
